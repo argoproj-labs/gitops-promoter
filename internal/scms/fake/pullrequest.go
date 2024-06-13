6a5573f79391ff4a2d3b7783877778e90bbb64e4
Zach Aller <zachaller@users.noreply.github.com>
2024-06-13 17:26:44 -0500

    make fake github pr client mirror pullrequest pointer state
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- internal/scms/fake/pullrequest.go
+++ internal/scms/fake/pullrequest.go
@@ -2,9 +2,14 @@ package fake
 
 import (
 	"context"
+	"fmt"
+	"sync"
 
 	"github.com/zachaller/promoter/api/v1alpha1"
 )
+
+var pullRequests map[string]*v1alpha1.PullRequest
+var mutex sync.RWMutex
 
 type PullRequest struct {
 }
@@ -14,30 +19,55 @@ func NewFakePullRequestProvider() *PullRequest {
 }
 
 func (pr *PullRequest) Create(ctx context.Context, title, head, base, description string, pullRequest *v1alpha1.PullRequest) error {
-	pullRequest.Status.ID = "1"
-	return nil
+	pullRequest.Spec.Title = title
+	pullRequest.Spec.SourceBranch = head
+	pullRequest.Spec.TargetBranch = base
+	pullRequest.Spec.Description = description
+	return pr.savePointer(ctx, pullRequest)
 }
 
 func (pr *PullRequest) Update(ctx context.Context, title, description string, pullRequest *v1alpha1.PullRequest) error {
-	pullRequest.Status.ID = "1"
-	return nil
+	pullRequest.Spec.Title = title
+	pullRequest.Spec.Description = description
+	return pr.savePointer(ctx, pullRequest)
 }
 
 func (pr *PullRequest) Close(ctx context.Context, pullRequest *v1alpha1.PullRequest) error {
-	pullRequest.Status.ID = "1"
-	return nil
+	return pr.savePointer(ctx, pullRequest)
 }
 
 func (pr *PullRequest) Merge(ctx context.Context, commitMessage string, pullRequest *v1alpha1.PullRequest) error {
-	pullRequest.Status.ID = "1"
-	return nil
+	return pr.savePointer(ctx, pullRequest)
 }
 
 func (pr *PullRequest) FindOpen(ctx context.Context, pullRequest *v1alpha1.PullRequest) (bool, error) {
-	if (pullRequest.Spec.State == "closed" && pullRequest.Status.State == "closed") ||
-		(pullRequest.Spec.State == "merged" && pullRequest.Status.State == "merged") {
-		return false, nil
+	return pr.findOpen(ctx, pullRequest), nil
+}
+
+func (pr *PullRequest) savePointer(ctx context.Context, pullRequest *v1alpha1.PullRequest) error {
+	mutex.Lock()
+	defer mutex.Unlock()
+	if pullRequests == nil {
+		pullRequests = make(map[string]*v1alpha1.PullRequest)
 	}
-	pullRequest.Status.ID = "1"
-	return true, nil
+	if _, ok := pullRequests[pr.getMapKey(pullRequest)]; !ok {
+		pullRequest.Status.ID = fmt.Sprintf("%d", len(pullRequests)+1)
+		pullRequests[pr.getMapKey(pullRequest)] = pullRequest
+	}
+	return nil
+}
+
+func (pr *PullRequest) findOpen(ctx context.Context, pullRequest *v1alpha1.PullRequest) bool {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	if _, ok := pullRequests[pr.getMapKey(pullRequest)]; ok {
+		if pullRequests[pr.getMapKey(pullRequest)].Status.State == "open" {
+			return true
+		}
+	}
+	return false
+}
+
+func (pr *PullRequest) getMapKey(pullRequest *v1alpha1.PullRequest) string {
+	return fmt.Sprintf("%s/%s/%s", pullRequest.Spec.SourceBranch, pullRequest.Spec.TargetBranch, pullRequest.Status.ID)
 }
