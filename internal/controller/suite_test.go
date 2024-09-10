3fb2f06caa4a32f95a14100e27c230c1cec51583
Zach Aller <zachaller@users.noreply.github.com>
2024-09-10 17:09:47 -0500

    msg
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -306,7 +306,8 @@ func addPendingCommit(gitPath string, repoOwner string, repoName string) (string
 	Expect(err).NotTo(HaveOccurred())
 	_, err = runGitCmd(gitPath, "git", "commit", "-m", "added fake manifests commit with timestamp")
 	Expect(err).NotTo(HaveOccurred())
-	_, err = runGitCmd(gitPath, "git", "push", "-u", "origin", "main")
+	msg, err := runGitCmd(gitPath, "git", "push", "-u", "origin", "main")
+	fmt.Println(msg)
 	Expect(err).NotTo(HaveOccurred())
 
 	_, err = runGitCmd(gitPath, "git", "checkout", "-B", "environment/development-next")
