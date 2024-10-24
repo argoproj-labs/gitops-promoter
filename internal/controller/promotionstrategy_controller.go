93faf3a5051c57e8d768cbd6c848cf9dfb249a5b
Zach Aller <zachaller@users.noreply.github.com>
2024-10-24 15:43:50 -0700

    chore: add secret read (#83)
    * add secret read
    
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>
    
    * add secret read
    
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- internal/controller/promotionstrategy_controller.go
+++ internal/controller/promotionstrategy_controller.go
@@ -57,7 +57,7 @@ type PromotionStrategyReconciler struct {
 //+kubebuilder:rbac:groups=promoter.argoproj.io,resources=promotionstrategies/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=promoter.argoproj.io,resources=promotionstrategies/finalizers,verbs=update
 
-//+kubebuilder:rbac:groups="",resources=Secrets,verbs=get;list;watch
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
