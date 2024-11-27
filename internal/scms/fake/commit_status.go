2fafa1090a4406f91788ffed9ba3f653df54b611
Zach Aller <zachaller@users.noreply.github.com>
2024-11-27 08:59:00 -0600

    simplify fake commit
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- internal/scms/fake/commit_status.go
+++ internal/scms/fake/commit_status.go
@@ -3,17 +3,9 @@ package fake
 import (
 	"context"
 	"errors"
-	"fmt"
-	"sync"
-
 	promoterv1alpha1 "github.com/argoproj-labs/gitops-promoter/api/v1alpha1"
 	"github.com/argoproj-labs/gitops-promoter/internal/scms"
 	v1 "k8s.io/api/core/v1"
-)
-
-var (
-	commitStatuses map[string]*promoterv1alpha1.CommitStatus
-	mutexCS        sync.RWMutex
 )
 
 type CommitStatus struct{}
@@ -29,23 +21,6 @@ func (cs CommitStatus) Set(ctx context.Context, commitStatus *promoterv1alpha1.C
 		return nil, errors.New("sha is required")
 	}
 	commitStatus.Status.Phase = commitStatus.Spec.Phase
-	cs.savePointer(commitStatus)
-	return commitStatus, nil
-}
-
-func (cs *CommitStatus) savePointer(commitStatus *promoterv1alpha1.CommitStatus) {
-	mutexCS.Lock()
-	defer mutexCS.Unlock()
-	if commitStatuses == nil {
-		commitStatuses = make(map[string]*promoterv1alpha1.CommitStatus)
-	}
-	if _, ok := commitStatuses[cs.getMapKey(commitStatus)]; !ok {
-		commitStatus.Status.Id = fmt.Sprintf("%d", len(commitStatuses)+1)
-	}
 	commitStatus.Status.Sha = commitStatus.Spec.Sha
-	commitStatuses[cs.getMapKey(commitStatus)] = commitStatus
-}
-
-func (cs *CommitStatus) getMapKey(commitStatus *promoterv1alpha1.CommitStatus) string {
-	return fmt.Sprintf("%s/%s", commitStatus.Spec.Sha, commitStatus.Status.Id)
+	return commitStatus, nil
 }
