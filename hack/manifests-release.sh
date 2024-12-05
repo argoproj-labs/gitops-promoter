4dab5bae333ce1104ba029fdf8e1e964da89840e
Zach Aller <zachaller@users.noreply.github.com>
2024-12-05 16:11:16 -0600

    fix: fix image tag in release artifact (#149)
    * fix image tag
    
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>
    
    * fix getting started docs
    
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- hack/manifests-release.sh
+++ hack/manifests-release.sh
@@ -20,6 +20,6 @@ IMAGE_NAMESPACE="${IMAGE_NAMESPACE:-quay.io/argoprojlabs/gitops-promoter}"
 IMAGE_FQN="$IMAGE_NAMESPACE:$IMAGE_TAG"
 
 $KUSTOMIZE version
-cd "${SRCROOT}/config/release" && $KUSTOMIZE edit set image "argoproj-labs/gitops-promoter=${IMAGE_FQN}"
+cd "${SRCROOT}/config/release" && $KUSTOMIZE edit set image "quay.io/argoprojlabs/gitops-promoter=${IMAGE_FQN}"
 echo "${AUTOGENMSG}" > "${SRCROOT}/install.yaml"
 $KUSTOMIZE build "${SRCROOT}/config/release" >> "${SRCROOT}/install.yaml"
