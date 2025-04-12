d284e9140064568bac4cd9e2339a37778a870825
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2025-04-12 11:37:30 +0100

    feat(controller): prometheus metrics for git and SCM operations (#117)
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

---
+++ docs/metrics.md
@@ -0,0 +1,52 @@
+# Metrics
+
+GitOps Promoter produces metrics counter and histogram metrics for all network-bound operations.
+
+## git_operations_total
+
+A counter of git clone operations.
+
+Labels:
+
+* `repository`: The name of the GitRepository resource associated with the operation.
+* `scm_provider`: The name of the ScmProvider resource associated with the operation.
+* `operation`: The type of git operation (clone, fetch, pull, push, ls-remote).
+* `result`: Whether the operation succeeded (success, failure).
+
+## git_operations_duration_seconds
+
+A histogram of the duration of git clone operations.
+
+Labels:
+
+* `repository`: The name of the GitRepository resource associated with the operation.
+* `scm_provider`: The name of the ScmProvider resource associated with the operation.
+* `operation`: The type of git operation (clone, fetch, pull, push, ls-remote).
+* `result`: Whether the operation succeeded (success, failure).
+
+## scm_calls_total
+
+A counter of SCM API calls.
+
+Labels:
+
+* `repository`: The name of the GitRepository resource associated with the operation.
+* `scm_provider`: The name of the ScmProvider resource associated with the operation.
+* `api`: The SCM API being called (CommitStatus, PullRequest)
+* `operation`: The type of SCM operation.
+  * For CommitStatus, this is always create.
+  * For PullRequest, this is create, update, merge, close, or list.
+* `response_code`: The HTTP response code.
+
+## scm_calls_duration_seconds
+
+A histogram of the duration of SCM API calls.
+
+Labels:
+
+* `repository`: The name of the GitRepository resource associated with the operation.
+* `api`: The SCM API being called (CommitStatus, PullRequest)
+* `operation`: The type of SCM operation.
+  * For CommitStatus, this is always create.
+  * For PullRequest, this is create, update, merge, close, or list.
+* `response_code`: The HTTP response code.
---
+++ internal/metrics/metrics.go
@@ -0,0 +1,124 @@
+package metrics
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/argoproj-labs/gitops-promoter/api/v1alpha1"
+	"github.com/prometheus/client_golang/prometheus"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
+)
+
+type GitOperation string
+
+const (
+	GitOperationClone    GitOperation = "clone"
+	GitOperationFetch    GitOperation = "fetch"
+	GitOperationPull     GitOperation = "pull"
+	GitOperationPush     GitOperation = "push"
+	GitOperationLsRemote GitOperation = "ls-remote"
+)
+
+type GitOperationResult string
+
+const (
+	GitOperationResultSuccess GitOperationResult = "success"
+	GitOperationResultFailure GitOperationResult = "failure"
+)
+
+func GitOperationResultFromError(err error) GitOperationResult {
+	if err == nil {
+		return GitOperationResultSuccess
+	}
+	return GitOperationResultFailure
+}
+
+type SCMAPI string
+
+const (
+	SCMAPICommitStatus SCMAPI = "CommitStatus"
+	SCMAPIPullRequest  SCMAPI = "PullRequest"
+)
+
+type SCMOperation string
+
+const (
+	SCMOperationCreate SCMOperation = "create"
+	SCMOperationUpdate SCMOperation = "update"
+	SCMOperationMerge  SCMOperation = "merge"
+	SCMOperationClose  SCMOperation = "close"
+	SCMOperationList   SCMOperation = "list"
+)
+
+var (
+	gitOperationsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "git_operations_total",
+			Help: "A counter of git clone operations.",
+		},
+		[]string{"repository", "scm_provider", "operation", "result"},
+	)
+
+	gitOperationsDurationSeconds = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "git_operations_duration_seconds",
+			Help:    "A histogram of the duration of git clone operations.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"repository", "scm_provider", "operation", "result"},
+	)
+
+	scmCallsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "scm_calls_total",
+			Help: "A counter of SCM API calls.",
+		},
+		[]string{"repository", "scm_provider", "api", "operation", "response_code"},
+	)
+
+	scmCallsDurationSeconds = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "scm_calls_duration_seconds",
+			Help:    "A histogram of the duration of SCM API calls.",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"repository", "scm_provider", "api", "operation", "response_code"},
+	)
+
+	// IMPORTANT: If you add or update metrics, also update metrics.md.
+)
+
+func init() {
+	// Register custom metrics with the k8s controller-runtime metrics registry
+	metrics.Registry.MustRegister(
+		gitOperationsTotal,
+		gitOperationsDurationSeconds,
+		scmCallsTotal,
+		scmCallsDurationSeconds,
+	)
+}
+
+// RecordGitOperation records both the increment and observation for git operations.
+func RecordGitOperation(gitRepo *v1alpha1.GitRepository, operation GitOperation, result GitOperationResult, duration time.Duration) {
+	labels := prometheus.Labels{
+		"repository":   gitRepo.Name,
+		"scm_provider": gitRepo.Spec.ScmProviderRef.Name,
+		"operation":    string(operation),
+		"result":       string(result),
+	}
+	gitOperationsTotal.With(labels).Inc()
+	gitOperationsDurationSeconds.With(labels).Observe(duration.Seconds())
+}
+
+// RecordSCMCall records both the increment and observation for SCM API calls.
+func RecordSCMCall(gitRepo *v1alpha1.GitRepository, api SCMAPI, operation SCMOperation, responseCode int, duration time.Duration) {
+	labels := prometheus.Labels{
+		"repository":    gitRepo.Name,
+		"scm_provider":  gitRepo.Spec.ScmProviderRef.Name,
+		"api":           string(api),
+		"operation":     string(operation),
+		"response_code": strconv.Itoa(responseCode),
+	}
+	scmCallsTotal.With(labels).Inc()
+	scmCallsDurationSeconds.With(labels).Observe(duration.Seconds())
+}
