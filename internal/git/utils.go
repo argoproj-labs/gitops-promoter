464fa3e2995a94328fcf32284c42b5935dab5280
Zach Aller <zachaller@users.noreply.github.com>
2025-07-25 16:03:40 -0500

    comments
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- internal/git/utils.go
+++ internal/git/utils.go
@@ -7,6 +7,8 @@ import (
 	"golang.org/x/net/context"
 )
 
+// containsYamlFileSuffix Check if the provided list of files contains any file with a .yaml or .yml suffix.
+// TODO: This is temporary check we should add some path globbing support to the specs
 func containsYamlFileSuffix(ctx context.Context, files []string) bool {
 	logger := logr.FromContextOrDiscard(ctx)
 	for _, file := range files {
