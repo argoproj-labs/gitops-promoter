cb471a303155bfb58b54c7b0d5b044e4bc80df93
Zach Aller <zachaller@users.noreply.github.com>
2025-08-18 09:48:20 -0500

    fix: make optional
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- api/v1alpha1/commitstatus_types.go
+++ api/v1alpha1/commitstatus_types.go
@@ -59,9 +59,12 @@ type CommitStatusStatus struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Id is the unique identifier of the commit status, set by the SCM
-	Id    string            `json:"id"`
-	Sha   string            `json:"sha"`
-	Phase CommitStatusPhase `json:"phase"`
+	Id  string `json:"id"`
+	Sha string `json:"sha"`
+	// +kubebuilder:default:=pending
+	// +kubebuilder:validation:Enum:=pending;success;failure;
+	// +kubebuilder:validation:Optional
+	Phase CommitStatusPhase `json:"phase,omitempty"`
 
 	// Conditions Represents the observations of the current state.
 	// +patchMergeKey=type
