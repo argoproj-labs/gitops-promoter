21452d866caf20562fb28693eeb2bf6b6d7773f8
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2025-10-15 11:10:39 -0400

    chore: improve missing branch error (#587)
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

---
+++ internal/git/git_test.go
@@ -0,0 +1,98 @@
+package git_test
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestGit(t *testing.T) {
+	t.Parallel()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Git Suite")
+}
+
+// Helper function to run git commands
+func runGitCmd(dir string, args ...string) (string, error) {
+	cmd := exec.Command("git", args...)
+	cmd.Dir = dir
+	cmd.Env = append(os.Environ(), "GIT_TERMINAL_PROMPT=0")
+	output, err := cmd.CombinedOutput()
+	return string(output), err
+}
+
+var _ = Describe("GetBranchShas", func() {
+	var tempRepoDir string
+
+	BeforeEach(func() {
+		// Create a temporary directory for the test repository
+		var err error
+		tempRepoDir, err = os.MkdirTemp("", "git-test-*")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		if tempRepoDir != "" {
+			Expect(os.RemoveAll(tempRepoDir)).To(Succeed())
+		}
+	})
+
+	Context("When the branch does not exist on the remote", func() {
+		It("should provide a clear error message", func() {
+			By("Setting up a bare git repository")
+			_, err := runGitCmd(tempRepoDir, "init", "--bare")
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Creating an initial commit")
+			workDir, err := os.MkdirTemp("", "git-work-*")
+			Expect(err).NotTo(HaveOccurred())
+			defer func() {
+				Expect(os.RemoveAll(workDir)).To(Succeed())
+			}()
+
+			_, err = runGitCmd(workDir, "clone", tempRepoDir, ".")
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = runGitCmd(workDir, "config", "user.name", "Test User")
+			Expect(err).NotTo(HaveOccurred())
+			_, err = runGitCmd(workDir, "config", "user.email", "test@example.com")
+			Expect(err).NotTo(HaveOccurred())
+
+			err = os.WriteFile(filepath.Join(workDir, "hydrator.metadata"), []byte(`{"drySha": "abc123"}`), 0644)
+			Expect(err).NotTo(HaveOccurred())
+			_, err = runGitCmd(workDir, "add", "hydrator.metadata")
+			Expect(err).NotTo(HaveOccurred())
+			_, err = runGitCmd(workDir, "commit", "-m", "Initial commit")
+			Expect(err).NotTo(HaveOccurred())
+
+			defaultBranch, err := runGitCmd(workDir, "rev-parse", "--abbrev-ref", "HEAD")
+			Expect(err).NotTo(HaveOccurred())
+			defaultBranch = strings.TrimSpace(defaultBranch)
+
+			_, err = runGitCmd(workDir, "push", "origin", defaultBranch)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cloning the repository")
+			cloneDir, err := os.MkdirTemp("", "git-clone-*")
+			Expect(err).NotTo(HaveOccurred())
+			defer func() {
+				Expect(os.RemoveAll(cloneDir)).To(Succeed())
+			}()
+
+			_, err = runGitCmd(cloneDir, "clone", "--filter=blob:none", tempRepoDir, ".")
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Attempting to fetch a non-existent branch")
+			nonExistentBranch := "environments/qal-usw2-eks-next"
+			output, err := runGitCmd(cloneDir, "fetch", "origin", nonExistentBranch)
+
+			Expect(err).To(HaveOccurred())
+			Expect(output).To(ContainSubstring("couldn't find remote ref"))
+		})
+	})
+})
