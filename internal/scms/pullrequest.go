652d89750d52ebf272cdf0b23961ff4520011de2
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2025-10-20 16:09:30 -0400

    chore: better communicate missing ID errors (#600)
    * chore: better communicate missing ID errors
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    * tweaks
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    * docs
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    * lint
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- internal/scms/pullrequest.go
+++ internal/scms/pullrequest.go
@@ -11,10 +11,13 @@ type PullRequestProvider interface {
 	// Create creates a new pull request with the specified title, head, base, and description.
 	Create(ctx context.Context, title, head, base, description string, pullRequest v1alpha1.PullRequest) (string, error)
 	// Close closes an existing pull request.
+	// pullRequest.Status.ID is guaranteed to be set when this is called.
 	Close(ctx context.Context, pullRequest v1alpha1.PullRequest) error
 	// Update updates an existing pull request with the specified title, description, and pull request details.
+	// pullRequest.Status.ID is guaranteed to be set when this is called.
 	Update(ctx context.Context, title, description string, pullRequest v1alpha1.PullRequest) error
 	// Merge merges an existing pull request with the specified commit message.
+	// pullRequest.Status.ID is guaranteed to be set when this is called.
 	Merge(ctx context.Context, pullRequest v1alpha1.PullRequest) error
 	// FindOpen checks if a pull request is open and returns its status.
 	FindOpen(ctx context.Context, pullRequest v1alpha1.PullRequest) (bool, v1alpha1.PullRequestCommonStatus, error)
