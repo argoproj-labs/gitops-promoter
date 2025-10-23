57659553315a6f26a0beb8a7ae3d762b665dc354
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2025-10-23 17:07:00 -0400

    handle forgejo
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- internal/scms/forgejo/pullrequest.go
+++ internal/scms/forgejo/pullrequest.go
@@ -170,9 +170,10 @@ func (pr *PullRequest) Merge(ctx context.Context, prObj promoterv1alpha1.PullReq
 	}
 
 	options := forgejo.MergePullRequestOption{
-		Style:        forgejo.MergeStyleMerge, // TODO: make the merge style configurable
-		Message:      prObj.Spec.Commit.Message,
-		HeadCommitId: prObj.Spec.MergeSha,
+		Style:                  forgejo.MergeStyleMerge, // TODO: make the merge style configurable
+		Message:                prObj.Spec.Commit.Message,
+		HeadCommitId:           prObj.Spec.MergeSha,
+		DeleteBranchAfterMerge: false, // We need the branch to stick around for the PR to keep pushing to.
 	}
 
 	start := time.Now()
