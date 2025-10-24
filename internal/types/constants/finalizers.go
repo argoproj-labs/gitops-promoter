007b876fb20ee3332d225e21da8cfbf9bfb6ed1a
Zach Aller <zachaller@users.noreply.github.com>
2025-10-24 10:37:47 -0500

    remove un-used fie
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- internal/types/constants/finalizers.go
+++
@@ -1,6 +0,0 @@
-package constants
-
-const (
-	// PullRequestFinalizer is the finalizer used to ensure PullRequest resources are properly cleaned up.
-	PullRequestFinalizer = "pullrequest.promoter.argoporoj.io/finalizer"
-)
