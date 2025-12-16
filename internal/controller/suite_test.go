f06235badd7126f8e802d09cc54c376d7ff87f10
Zach Aller <zachaller@users.noreply.github.com>
2025-12-15 21:51:46 -0600

    lint
    Signed-off-by: Zach Aller <zachaller@users.noreply.github.com>

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -289,74 +289,74 @@ var _ = BeforeSuite(func() {
 					},
 				},
 			},
-		TimedCommitStatus: promoterv1alpha1.TimedCommitStatusConfiguration{
-			WorkQueue: promoterv1alpha1.WorkQueue{
-				RequeueDuration:         metav1.Duration{Duration: time.Second * 1},
-				MaxConcurrentReconciles: 10,
-				RateLimiter: promoterv1alpha1.RateLimiter{
-					MaxOf: []promoterv1alpha1.RateLimiterTypes{
-						{
-							Bucket: &promoterv1alpha1.Bucket{
-								Qps:    10,
-								Bucket: 100,
+			TimedCommitStatus: promoterv1alpha1.TimedCommitStatusConfiguration{
+				WorkQueue: promoterv1alpha1.WorkQueue{
+					RequeueDuration:         metav1.Duration{Duration: time.Second * 1},
+					MaxConcurrentReconciles: 10,
+					RateLimiter: promoterv1alpha1.RateLimiter{
+						MaxOf: []promoterv1alpha1.RateLimiterTypes{
+							{
+								Bucket: &promoterv1alpha1.Bucket{
+									Qps:    10,
+									Bucket: 100,
+								},
+							},
+							{
+								ExponentialFailure: &promoterv1alpha1.ExponentialFailure{
+									BaseDelay: metav1.Duration{Duration: time.Millisecond * 5},
+									MaxDelay:  metav1.Duration{Duration: time.Minute * 1},
+								},
 							},
 						},
-						{
-							ExponentialFailure: &promoterv1alpha1.ExponentialFailure{
-								BaseDelay: metav1.Duration{Duration: time.Millisecond * 5},
-								MaxDelay:  metav1.Duration{Duration: time.Minute * 1},
+					},
+				},
+			},
+			WebRequestCommitStatus: promoterv1alpha1.WebRequestCommitStatusConfiguration{
+				WorkQueue: promoterv1alpha1.WorkQueue{
+					RequeueDuration:         metav1.Duration{Duration: time.Second * 1},
+					MaxConcurrentReconciles: 10,
+					RateLimiter: promoterv1alpha1.RateLimiter{
+						MaxOf: []promoterv1alpha1.RateLimiterTypes{
+							{
+								Bucket: &promoterv1alpha1.Bucket{
+									Qps:    10,
+									Bucket: 100,
+								},
+							},
+							{
+								ExponentialFailure: &promoterv1alpha1.ExponentialFailure{
+									BaseDelay: metav1.Duration{Duration: time.Millisecond * 5},
+									MaxDelay:  metav1.Duration{Duration: time.Minute * 1},
+								},
+							},
+						},
+					},
+				},
+			},
+			GitCommitStatus: promoterv1alpha1.GitCommitStatusConfiguration{
+				WorkQueue: promoterv1alpha1.WorkQueue{
+					RequeueDuration:         metav1.Duration{Duration: time.Minute * 5},
+					MaxConcurrentReconciles: 10,
+					RateLimiter: promoterv1alpha1.RateLimiter{
+						MaxOf: []promoterv1alpha1.RateLimiterTypes{
+							{
+								Bucket: &promoterv1alpha1.Bucket{
+									Qps:    10,
+									Bucket: 100,
+								},
+							},
+							{
+								ExponentialFailure: &promoterv1alpha1.ExponentialFailure{
+									BaseDelay: metav1.Duration{Duration: time.Millisecond * 5},
+									MaxDelay:  metav1.Duration{Duration: time.Minute * 1},
+								},
 							},
 						},
 					},
 				},
 			},
 		},
-		WebRequestCommitStatus: promoterv1alpha1.WebRequestCommitStatusConfiguration{
-			WorkQueue: promoterv1alpha1.WorkQueue{
-				RequeueDuration:         metav1.Duration{Duration: time.Second * 1},
-				MaxConcurrentReconciles: 10,
-				RateLimiter: promoterv1alpha1.RateLimiter{
-					MaxOf: []promoterv1alpha1.RateLimiterTypes{
-						{
-							Bucket: &promoterv1alpha1.Bucket{
-								Qps:    10,
-								Bucket: 100,
-							},
-						},
-						{
-							ExponentialFailure: &promoterv1alpha1.ExponentialFailure{
-								BaseDelay: metav1.Duration{Duration: time.Millisecond * 5},
-								MaxDelay:  metav1.Duration{Duration: time.Minute * 1},
-							},
-						},
-					},
-				},
-			},
-		},
-		GitCommitStatus: promoterv1alpha1.GitCommitStatusConfiguration{
-			WorkQueue: promoterv1alpha1.WorkQueue{
-				RequeueDuration:         metav1.Duration{Duration: time.Minute * 5},
-				MaxConcurrentReconciles: 10,
-				RateLimiter: promoterv1alpha1.RateLimiter{
-					MaxOf: []promoterv1alpha1.RateLimiterTypes{
-						{
-							Bucket: &promoterv1alpha1.Bucket{
-								Qps:    10,
-								Bucket: 100,
-							},
-						},
-						{
-							ExponentialFailure: &promoterv1alpha1.ExponentialFailure{
-								BaseDelay: metav1.Duration{Duration: time.Millisecond * 5},
-								MaxDelay:  metav1.Duration{Duration: time.Minute * 1},
-							},
-						},
-					},
-				},
-			},
-		},
-	},
-}
+	}
 	Expect(k8sClient.Create(ctx, controllerConfiguration)).To(Succeed())
 
 	settingsMgr := settings.NewManager(k8sManager.GetClient(), k8sManager.GetAPIReader(), settings.ManagerConfig{
