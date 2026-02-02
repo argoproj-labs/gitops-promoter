e2dd4029709fdc8037416c2fd1c0d62a0e01f424
emirot <emirot.nolan@gmail.com>
2026-02-02 09:18:22 -0800

    fix(rbac): events to API group permission manager role
    Signed-off-by: emirot <emirot.nolan@gmail.com>

--- internal/controller/promotionstrategy_controller.go
+++ internal/controller/promotionstrategy_controller.go
@@ -74,6 +74,7 @@ type PromotionStrategyReconciler struct {
 
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 //+kubebuilder:rbac:groups="",resources=events,verbs=create;patch
+//+kubebuilder:rbac:groups=events.k8s.io,resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
