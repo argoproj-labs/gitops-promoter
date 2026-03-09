d64209ffbf2808716a44f3a28c2cafa1c320a014
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2026-03-09 10:52:10 -0400

    satisfy nilaway
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- internal/git/git.go
+++ internal/git/git.go
@@ -197,7 +197,7 @@ func sanitizeReferences(ctx context.Context, refs []v1alpha1.RevisionReference) 
 			if validURL {
 				// ParseRequestURI is what k8s CEL validation uses: https://github.com/kubernetes/apiextensions-apiserver/blob/ab2ddc498e31f2701200bff261e89120b3d929c3/pkg/apiserver/schema/cel/library/urls.go#L233
 				u, err := url.ParseRequestURI(c.RepoURL)
-				validURL = err == nil && (u.Scheme == "http" || u.Scheme == "https")
+				validURL = err == nil && u != nil && (u.Scheme == "http" || u.Scheme == "https")
 			}
 			if validURL {
 				continue
