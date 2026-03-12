08f881ae867014bc495a0a7cf1c63a2789663bf5
Jonathan Winters <wintersjonathan0@gmail.com>
2026-03-12 10:26:22 -0700

    label must be true
    Signed-off-by: Jonathan Winters <wintersjonathan0@gmail.com>

--- ui/extension/showExtension.ts
+++ ui/extension/showExtension.ts
@@ -3,9 +3,9 @@ import type { Application } from '@shared/types/extension';
 export const LABEL = 'promoter.argoproj.io/has-promotionstrategy';
 
 export const showExtension = (application: Application): boolean => {
-  if (application.metadata.labels?.[LABEL]) return true;
-  const resources = application.status?.resources?.filter(
-    (r) => r.kind === 'PromotionStrategy' && r.group === 'promoter.argoproj.io',
-  );
-  return (resources?.length || 0) >= 1;
+	if (application.metadata.labels?.[LABEL]) return application.metadata.labels[LABEL] === 'true';
+	const resources = application.status?.resources?.filter(
+		(r) => r.kind === 'PromotionStrategy' && r.group === 'promoter.argoproj.io',
+	);
+	return (resources?.length || 0) >= 1;
 };
