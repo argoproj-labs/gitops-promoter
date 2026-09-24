22d68ff1f9be59fbd4c70a58f67f41e817b97867
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2026-09-24 15:49:58 -0400

    lint
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>

--- internal/git/git.go
+++ internal/git/git.go
@@ -562,7 +562,7 @@ func (g *EnvironmentOperations) HasConflict(ctx context.Context, proposedBranch,
 		// that keeps its tree and records both branch tips as parents. That commit establishes
 		// the merge-base required by the subsequent pull request.
 		//
-		// NOTE: we're intentionally taking on the risk of blowing away someone's unrelated 
+		// NOTE: we're intentionally taking on the risk of blowing away someone's unrelated
 		// proposed branch contents when adopted by Promoter. While that's always a possibility,
 		// the likelihood that we're making a mistake is higher with two branches that don't
 		// even share a history.
